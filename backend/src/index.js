bcf08a0a3b78227f11a80a0066ebcaa36c8eeb1f
DEVAANSH001 <theman3185@gmail.com>
2026-03-13 18:30:44 +0530

    feat: Open cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,10 +13,7 @@ const app  = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL || "https://mycal-lyart.vercel.app",
-    "http://localhost:3000",
-  ],
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
